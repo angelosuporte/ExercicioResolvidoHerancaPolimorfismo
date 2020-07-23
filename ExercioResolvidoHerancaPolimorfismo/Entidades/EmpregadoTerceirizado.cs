9c06b1d922ae73f7b68939bd5d6995f3b9896fd3
Angelo Carvalho <Angelo Carvalho>
2020-07-23 13:29:30 -0300

    Finalizando com implementação da classe Program

--- ExercioResolvidoHerancaPolimorfismo/Entidades/EmpregadoTerceirizado.cs
+++ ExercioResolvidoHerancaPolimorfismo/Entidades/EmpregadoTerceirizado.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ExercioResolvidoHerancaPolimorfismo.Entidades
+﻿namespace ExercioResolvidoHerancaPolimorfismo.Entidades
 {
-    class EmpregadoTerceirizado
+    class EmpregadoTerceirizado : Empregado
     {
+        public double DespesaAdicional { get; set; }
+
+        public EmpregadoTerceirizado()
+        {
+        }
+
+        public EmpregadoTerceirizado(string nome, int horas, double valorPorHora, double despesaAdicional) 
+            : base (nome, horas, valorPorHora) //<--Aproveitando o construtor da superclasse
+        {
+            DespesaAdicional = despesaAdicional;
+        }
+
+        public override double Pagamento()
+        {
+            return base.Pagamento() + 1.1 * DespesaAdicional;
+        }
     }
 }
--- ExercioResolvidoHerancaPolimorfismo/Program.cs
+++ ExercioResolvidoHerancaPolimorfismo/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using ExercioResolvidoHerancaPolimorfismo.Entidades;
+using System.Globalization;
 
 namespace ExercioResolvidoHerancaPolimorfismo
 {
@@ -6,7 +9,43 @@ namespace ExercioResolvidoHerancaPolimorfismo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Empregado> empregados = new List<Empregado>();
+
+            Console.Write("Informe o número de funcionário: ");
+            int numeroFuncionario = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numeroFuncionario; i++)
+            {
+                Console.WriteLine($"Dados do empregado #{i}: ");
+                Console.Write("Empregado Terceirizado (y/n)? ");
+                char simOuNao = char.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Horas: ");
+                int horas = int.Parse(Console.ReadLine());
+                Console.Write("Valor por hora: ");
+                double valorPorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (simOuNao == 'y')
+                {
+                    Console.Write("Despesa adicional: ");
+                    double despesaAdicional = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    empregados.Add(new EmpregadoTerceirizado(nome, horas, valorPorHora, despesaAdicional));
+                }
+                else
+                {
+                    empregados.Add(new Empregado(nome, horas, valorPorHora));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Pagamentos: ");
+            foreach (Empregado empregado in empregados)
+            {
+                Console.WriteLine(empregado.Nome + " - $ " + empregado.Pagamento().ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+          
+
         }
     }
 }
